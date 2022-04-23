a9243dcff2b662e745486cfedf3cb6c51b022770
pravarjain19 <pravarjain881@gmail.com>
2022-04-23 11:29:16 +0530

    Update reverseAnumber.java

--- reverseAnumber.java
+++ reverseAnumber.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class reverseAnumber {
     public static void main(String[] args) {
@@ -13,6 +14,10 @@ public class reverseAnumber {
         int temp = n ;
         int num = 0;
          while(temp!=0){
+
+            if( temp> Integer.MAX_VALUE/10 || temp<Integer.MIN_VALUE/10){   //for the out of range value 
+                return 0;
+            }
             int lastnum = temp%10; 
             num = num *10 + lastnum;
             temp/=10;
